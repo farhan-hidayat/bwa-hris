85f66e02de14c6d5b922b4b3f09e05531db26aec
farhan-hidayat <farhanarchman@gmail.com>
2022-11-06 16:53:02 +0700

    installasi jetstream, hubungan antar model & helper API

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,13 @@ Route::get('/', function () {
 });
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/admin', [AdminController::class, 'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
